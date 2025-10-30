81b77003233549549570a02fdbd15f883cc48afc
jaypeche <jaypeche@gmail.com>
2025-10-31 00:11:42 +0100

    Cleaning unused headers

--- notify_send.c
+++ notify_send.c
@@ -3,17 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-
-#include <sys/inotify.h>
 #include <libnotify/notify.h>
 
 void show_notify_loading(void) {
-
 		static char *icon = "/usr/share/pixmaps/clamav-realtime.png";
 
 	/* Send Loading Notification */
@@ -25,19 +17,17 @@ void show_notify_loading(void) {
 }
 
 void show_notify_nothreat(void) {
-
 		static char *icon = "/usr/share/pixmaps/clamav-realtime.png";
 
 	/* Send nothreat notification */
 
 		printf ("\a");
 		notify_init ("ClamAV Realtime");
-		NotifyNotification * nothreat = notify_notification_new ("ClamAV Realtime", "No threat found !", icon);
+		NotifyNotification * nothreat = notify_notification_new ("Clam,AV Realtime", "No threat found !", icon);
 		notify_notification_show (nothreat, NULL);
 }
 
 void show_notify_infected(void) {
-
 			static char *icon = "/usr/share/pixmaps/clamav-realtime.png";
 
 	/* Send Notification */
